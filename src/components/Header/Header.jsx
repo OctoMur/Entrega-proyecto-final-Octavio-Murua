59c5920717361c90d9df127d8ade472b7f50b77d
octavio <octavioAISM@gmail.com>
2023-11-03 12:53:39 -0300

    Entrega final: correcciones para vercel

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import NavBar from "../navBar/NavBar"
+import CategoryContaner from "../CategoryContaner/CategoryContaner"
+
+
+
+function Header({}) {
+    return (
+        <>
+            <NavBar/>
+            <CategoryContaner/>
+
+        </>
+    )
+}
+
+export default Header
